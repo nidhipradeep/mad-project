c89086336d873655fc1c57ad41284231f6152472
carbonxx <vishnunv142000@gmail.com>
2022-07-07 18:09:43 +0530

    upload

---
+++ app/src/main/java/com/example/scholarshipapplication/premetric.java
@@ -0,0 +1,15 @@
+package com.example.scholarshipapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class premetric extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_premetric);
+    }
+}
